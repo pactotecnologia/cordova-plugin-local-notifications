26f640f7fdeeea6a76cf78b235bcc64f0d9d629c
s0ckz <s0ckzz@gmail.com>
2014-10-15 21:56:01 -0300

    Implementing screen unlock and wake device on

--- src/android/ReceiverActivity.java
+++ src/android/ReceiverActivity.java
@@ -25,9 +25,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.app.KeyguardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class ReceiverActivity extends Activity {
 
@@ -35,6 +38,18 @@ public class ReceiverActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set window flags to unlock screen. This works on most devices by itself.
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+
+        // On most other devices, using the KeyguardManager + the permission in
+        // AndroidManifest.xml will do the trick
+        KeyguardManager mKeyGuardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+        KeyguardManager.KeyguardLock mLock = mKeyGuardManager.newKeyguardLock("Unlock");
+        mLock.disableKeyguard();
 
         Intent intent = this.getIntent();
         Bundle bundle = intent.getExtras();
